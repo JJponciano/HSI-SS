c61a354ceb6c822b242fd546b9f7b9a9005cd887
Dr. Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2023-04-21 11:04:09 +0200

    Update SemanticSegmentation.py

--- hiss/SemanticSegmentation.py
+++ hiss/SemanticSegmentation.py
@@ -1,9 +1,12 @@
+import os
+
 from hiss.keras_segmentation.models.pspnet import pspnet
-from hiss import keras_segmentation as predict
+from hiss.keras_segmentation.pretrained import model_from_checkpoint_path
+from hiss.keras_segmentation.predict import predict_multiple
 
 
 class SemanticSegmentation:
-    def __init__(self, model_class='pspnet', num_classes=51, channel=3, epochs=20, input_height=192, input_width=192,path=None, best_size=True):
+    def __init__(self, model_class='pspnet', num_classes=51, channel=3, epochs=20, input_height=192, input_width=192, best_size=True):
         if best_size:
             # compute the euclidian divition (without the rest and multiply by 192, a requirement for pspnet is to be a multiple of 192
             self.input_height = int(input_height/192)*192
@@ -17,15 +20,11 @@ class SemanticSegmentation:
         self.epochs = epochs
 
         self.model = pspnet(n_classes=self.num_classes , input_height=self.input_height, input_width=self.input_width, channels=self.channel)
-        if path is not None:
-            self.path=path
-        else:
-            self.path = self.model_class+ str(input_height)+"_"+ str(input_width)+".h5"
 
-    def train(self,train_dir_img,train_dir_annotations):
+
+    def train(self,train_dir_img,train_dir_annotations,path):
         self.model.train(train_images=train_dir_img, train_annotations=train_dir_annotations, epochs=self.epochs)
-        self.model.save(self.path)
-        return self.path
+        self.model.save(path)
 
     def load(self,path):
         model_config = {
@@ -40,5 +39,5 @@ class SemanticSegmentation:
         return self.model.evaluate_segmentation(inp_images_dir=img_dir, annotations_dir=annotations_dir)
 
     def prediction(self,input_dir,output_dir):
-        predict.predict_multiple(self.model, inp_dir=input_dir, out_dir=output_dir)
+        predict_multiple(self.model, inp_dir=input_dir, out_dir=output_dir)
 
